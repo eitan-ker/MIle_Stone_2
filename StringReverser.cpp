8aa57f5bf13229c380ab71851ebb9175e070d6d0
menimen <meni.ashurov@gmail.com>
2020-01-14 23:29:13 +0200

    started working on CacheManger and created solve() function in StringRevereser class to return reversed string given as a parameter

---
+++ StringReverser.cpp
@@ -0,0 +1,15 @@
+//
+// Created by meni on 14/01/2020.
+//
+#include <string>
+#include "StringReverser.h"
+using namespace std;
+string StringReverser::solve(string problem) { //reverse the given string as a parameter
+  int i = problem.length() - 1;
+  const char* token = &problem[i];
+  string s = "\0";
+  while(i >= 0) {
+    s+=*token;
+    token--;
+  }
+}
