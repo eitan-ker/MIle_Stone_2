7ea057055f1c73653b4a714552c9e5ce4baea041
eitan-ker <eizzker@gmail.com>
2020-01-18 13:22:40 +0200

    need you to fix bugs so the matrix will be initialized

---
+++ MatrixConverter.h
@@ -0,0 +1,102 @@
+//
+// Created by eizzker on 17/01/2020.
+//
+
+#ifndef MILE_STONE_2_MATRIXCONVERTER_H
+#define MILE_STONE_2_MATRIXCONVERTER_H
+
+#include <sstream>
+#include "Searchable.h"
+#include "Solver.h"
+#include "State.h"
+template<class T>
+
+class MatrixConverter : public Searcheable<T> {
+private:
+    vector<vector<State<int>>> _matrix;
+public :
+    // make matrix
+    int countLength(string problem) {
+        std::string s = problem;
+        std::string delimiter = "\n", delimiter2 = ",";
+        int counter = 0;
+        size_t pos = 0, pos2 = 0;
+        std::string token, token2;
+        while ((pos = s.find(delimiter)) != std::string::npos) {
+            token = s.substr(0, pos);
+            token = token + ",";
+            while ((pos2 = token.find(delimiter2)) != std::string::npos) {
+                token2 = token.substr(0, pos2);
+                counter++;
+                token.erase(0, pos2 + delimiter2.length());
+            }
+            break;
+        }
+        return counter;
+}
+
+    vector<vector<State<T>>> initializeArray(string matr, int lineLength, vector <vector<State<int>>> matrix) {
+        int i = 0, r = 0, c = 0, matrLength = matr.length();
+        string line = "";
+        vector<int> stringValues(lineLength);
+        for (i = 0; i < matrLength; i++) {
+            if (r == lineLength) {
+                break;
+            }
+            if (matr[i] == '\n') {
+                stringValues = onlyValues(line + ",", lineLength);
+                for (c = 0; c < lineLength; c++) {
+                    stringstream rr, cc;
+                     rr << r;
+                     cc << c;
+                    string rs = rr.str();
+                    string cs = cc.str();
+                    string state = rs + cs;
+                    matrix[r][c] = new State<T>(state, stringValues[c]);
+                }
+                r++;
+                line = "";
+                continue;
+            }
+            line = line + matr[i];
+
+        }
+        return matrix;
+}
+
+    vector<int> onlyValues(string stringBuf, int length) {
+        std::string s = stringBuf;
+        std::string delimiter = ",";
+        int counter = 0;
+        size_t pos = 0;
+        std::string token;
+        vector<int> stringValues;
+        int tempNum = 0;
+        while ((pos = s.find(delimiter)) != std::string::npos) {
+            token = s.substr(0, pos);
+            stringstream convert(token);
+            convert >> tempNum;
+            stringValues.push_back(tempNum);
+            s.erase(0, pos + delimiter.length());
+        }
+        return stringValues;
+}
+
+    MatrixConverter(string problem) {
+        int lineLength = countLength(problem);
+        vector<vector<T>> matrix(lineLength, vector<int>(lineLength));
+        this->_matrix = initializeArray(problem, lineLength, matrix);
+    }
+
+    State<T> getInitialState() {
+}
+
+    bool isGoalState(State<T> state) {
+}
+
+    vector<State<T>> getAllPossibleStates(State<T> state) {
+}
+    };
+
+
+#endif //MILE_STONE_2_MATRIXCONVERTER_H
--- State.h
+++ State.h
@@ -4,20 +4,25 @@
 
 #ifndef MILE_STONE2__STATE_H_
 #define MILE_STONE2__STATE_H_
-template <class T>
+
+template<class T>
 class State {
- private :
-  T _state; // the state represented by string
-  double _cost // cost to reach this state
-  State<T> _cameFrom; //the state we came from to this state
-  public:
-  State(State<T> state) {
-    this->_state = state;
-    this->_cameFrom = nullptr;
-  };
-  bool Equals(State<T> s) {
-    return this->_state.Eqals(s._state);
-  }
-  bool isValid(State<T> s)
+private :
+    T _state; // the state represented by string
+    double _cost; // cost to reach this state
+    State<T> _cameFrom; //the state we came from to this state
+public:
+    State(T state, double cost) {
+        this->_cost = cost;
+        this->_state = state;
+        this->_cameFrom = this;
+    };
+
+    bool Equals(State<T> s) {
+        return this->_state.Eqals(s._state);
+    }
+
+    bool isValid(State<T> s);
 };
+
 #endif //MILE_STONE2__STATE_H_
