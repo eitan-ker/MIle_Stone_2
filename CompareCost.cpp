39b3235df274feeebf22f588d7bdcecb994087a7
menimen <meni.ashurov@gmail.com>
2020-01-22 01:33:37 +0200

    added comparator for BestFirstSearch to know which State is the minimum in priority queue

---
+++ CompareCost.cpp
@@ -0,0 +1,12 @@
+#include "State.h"
+//
+// Created by meni on 21/01/2020.
+//
+template<class P>
+struct CompareCost {
+  bool operator()(State<P>* const& p1, State<P>* const& p2)
+  {
+    return p1->gettotalCost() > p2->gettotalCost();
+  }
+};
+
